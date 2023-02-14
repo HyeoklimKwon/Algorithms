51ad284f92f11ef5fd683482befd55624da455b2
HyeoklimKwon <85211424+HyeoklimKwon@users.noreply.github.com>
2023-02-14 11:44:24 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,19 +1,19 @@
 def solution(storey):
     answer = 0
-    while storey != 0:
-        n = storey % 10 
-
-        if n >= 6:
-            storey += 10 - n
+    while storey != 0 :
+        #앞자리 검사를 위해 
+        n = storey % 10
+        k = storey + 10 - n
+        k %= 100       
+        if n >= 6 :
             answer += 10 - n
-
+            storey += 10 - n
+        # 앞자리가 6 이상일 경우 
         elif n == 5 and (storey // 10) % 10 >= 5:
-            storey += 10 - n
             answer += 10 - n
-
-        else:
+            storey += 10 - n
+        else :
+            storey -= n
             answer += n
-        storey = storey // 10
-
-
+        storey //= 10
     return answer
