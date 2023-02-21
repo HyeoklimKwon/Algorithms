7647b8a6d660566594424aa8845180af1bca5317
HyeoklimKwon <85211424+HyeoklimKwon@users.noreply.github.com>
2023-02-21 17:18:06 +0900

    [level 3] Title: 등굣길, Time: 4.97 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -4,7 +4,7 @@ def solution(m, n, puddles):
     # 웅덩이 : puddles
     # print(m, n , puddles)
     map_list = [[0] * m for _ in range(n)]
-    map_list[0][0] = 1
+    
     # # 초기 설정
     # map_list[0] = [1]* m
     # for i in range(len(map_list)):
@@ -31,16 +31,5 @@ def solution(m, n, puddles):
 #                 #     map_list[i][j] = 1
     for line in map_list:
         print(*line)
-    
-    for i in range(n):
-        for j in range(m):
-            if map_list[i][j] == 0 :
-                if 0 < i :
-                    if map_list[i - 1][j] != 'X':
-                        map_list[i][j] += map_list[i - 1][j]
-                if 0 < j :
-                    if map_list[i][j - 1] != 'X':
-                        map_list[i][j] += map_list[i][j - 1]
-    
     answer = map_list[-1][-1]
-    return answer % 1000000007
+    return answer
